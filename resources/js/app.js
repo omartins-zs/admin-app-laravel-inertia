c15e38c0ba61fdece414bca22e94fbd2bc07fd68
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-27 12:16:15 -0300

    Inicializando o Inertia.js e o Vue 3 no Laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-import './bootstrap';
+import "./bootstrap";
+// import "../css/app.css";
+
+// import { createApp, h } from "vue";
+// import { createInertiaApp } from "@inertiajs/inertia-vue3";
+// import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+// createInertiaApp({
+//   title: (title) => `${title} - ${appName}`,
+//   resolve: (name) =>
+//     resolvePageComponent(
+//       `./Pages/${name}.vue`,
+//       import.meta.glob("./Pages/**/*.vue")
+//     ),
+//   setup({ el, app, props, plugin }) {
+//     return createApp({ render: () => h(app, props) })
+//       .use(plugin)
+//       .mount(el);
+//   },
+// });
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({ el, app, props, plugin }) {
+        createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
